c9d439b8655538312f738332d9468629e3b9a17a
gmanon <ggonjon@gmail.com>
2013-08-27 18:37:14 -0400

    index
    Index page control

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+**  Author: Guillermina Gonjon
+**  Date:   August 27th, 2013
+**/
+
+//  Check for errors
+
+//  Redirect on error event
+
+//  Open database
+
+//  Check on user permission
+
+//  Redirect to right application
+
+//  Close database
+?>
