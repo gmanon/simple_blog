4377909297487da88204ca8ec8c89e8976fcce09
GManon <ggonjon@gmail.com>
2017-03-25 23:06:39 -0400

    Create init.inc.php

---
+++ includes/init.inc.php
@@ -0,0 +1,29 @@
+<?php
+/*///////////////////////////////////////////////////////////////////////////////////////////////
+//Project Name: GMANON Simple_blog
+//File Name: Init
+//Author: Guillermina Gonjon
+//Developer Email: ggonjon@gmail.com
+//
+//GMANON PROPOSAL's only intention is to be useful to whoever wants it or need it.  
+//It's distributed totaly free.  Please do not modify the code without letting me know.  
+//Allow any modification to be added in order to improve the program to the 
+//benefit of all the users.  
+// 
+//    Copyright (C) 2017  Guillermina Manon-Gonjon (Alias GManon)
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////*/
+session_start();
+require_once('includes/functions.inc.php');
