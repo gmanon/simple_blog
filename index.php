0d3f705f7e3f54537cd1bad56b8572af9316397f
GManon <ggonjon@gmail.com>
2017-03-25 21:43:06 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,13 @@
 **  Author: Guillermina Gonjon
 **  Date:   August 27th, 2013
 **/
-
-//  Check for errors
+require_once "includes/init.inc.php";
 
 //  Redirect on error event
-
-//  Open database
 
 //  Check on user permission
 
 //  Redirect to right application
 
-//  Close database
+//  Close everything
 ?>
